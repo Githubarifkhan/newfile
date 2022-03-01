1a9e6d1f2f0b498263e27a39399ea07156f1bc41
Arif khan <786arifkhan0@gmail.com>
2022-03-01 16:11:56 +0530

    commiting

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        int a= 10;
+        int b =20;
+        System.out.println("Bharat mata ki jai");
+        System.out.println("Vande mataram"); 
+        System.out.println("Ayan khan"); 
     }
 }
